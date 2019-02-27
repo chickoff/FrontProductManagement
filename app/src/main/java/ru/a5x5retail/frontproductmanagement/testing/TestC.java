15e3216c2377dcdcbe58dc01b6e2985692d3e92e
chickoff <ksandrxxx@gmail.com>
2019-02-27 17:14:35 +0300

    Я коммичу первый раз!

--- app/src/main/java/ru/a5x5retail/frontproductmanagement/testing/TestC.java
+++ app/src/main/java/ru/a5x5retail/frontproductmanagement/testing/TestC.java
@@ -1,4 +1,13 @@
 package ru.a5x5retail.frontproductmanagement.testing;
 
 public class TestC {
+    private String str;
+
+    public String getStr() {
+        return str;
+    }
+
+    public void setStr(String str) {
+        this.str = str;
+    }
 }
