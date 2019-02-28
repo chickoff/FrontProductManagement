9dddf26e07c94bd4b79fe01f2e97f28313a0cbdf
s1 <lexx@5x5retail.ru>
2019-02-28 08:53:40 +0300

    труляля111

--- app/src/main/java/ru/a5x5retail/frontproductmanagement/testing/ttttt.java
+++ app/src/main/java/ru/a5x5retail/frontproductmanagement/testing/ttttt.java
@@ -1,15 +1,12 @@
 package ru.a5x5retail.frontproductmanagement.testing;
 
-import android.view.View;
+
 
 public class ttttt
-        implements View.OnClickListener
+
 {
 
 
-    @Override
-    public void onClick(View v) {
 
-    }
 }
 
