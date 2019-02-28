afee11984a40d5c3cf81f2c417c2901ecf68c3e6
chickoff <ksandrxxx@gmail.com>
2019-02-28 08:46:39 +0300

    Противоречие №1!

--- app/src/main/java/ru/a5x5retail/frontproductmanagement/testing/ttttt.java
+++ app/src/main/java/ru/a5x5retail/frontproductmanagement/testing/ttttt.java
@@ -2,7 +2,14 @@ package ru.a5x5retail.frontproductmanagement.testing;
 
 public class ttttt {
 
+    private int i;
 
+    public int getI() {
+        return i;
+    }
 
+    public void setI(int i) {
+        this.i = i;
+    }
 }
 
