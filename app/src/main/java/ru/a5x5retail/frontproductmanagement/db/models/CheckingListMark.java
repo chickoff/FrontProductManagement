5cba7d020ac93adcbd5e42aac2aa5c07cd275d36
chickoff <ksandrxxx@gmail.com>
2019-03-25 12:06:49 +0300

    по просьбе трудящихся и жаждущих

---
+++ app/src/main/java/ru/a5x5retail/frontproductmanagement/db/models/CheckingListMark.java
@@ -0,0 +1,12 @@
+package ru.a5x5retail.frontproductmanagement.db.models;
+
+import java.math.BigDecimal;
+
+public class CheckingListMark {
+    public String guid;
+    public String checkingListIncHeadGuid;
+    public int code;
+    public BigDecimal qtyCl;
+    public BigDecimal qtyTtn;
+    public int orderBy;
+}
