ce1540951016ede814412c662df3a4377464850d
chickoff <ksandrxxx@gmail.com>
2019-03-25 12:07:02 +0300

    по просьбе трудящихся и жаждущих

---
+++ app/src/main/java/ru/a5x5retail/frontproductmanagement/db/query/read/GetCheckingListIncMarkListQuery.java
@@ -0,0 +1,54 @@
+package ru.a5x5retail.frontproductmanagement.db.query.read;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.a5x5retail.frontproductmanagement.db.converters.CheckingListHeadConverter;
+import ru.a5x5retail.frontproductmanagement.db.converters.CheckingListMarkConverter;
+import ru.a5x5retail.frontproductmanagement.db.models.CheckingListHead;
+import ru.a5x5retail.frontproductmanagement.db.models.CheckingListMark;
+import ru.a5x5retail.frontproductmanagement.db.query.CallableQuery;
+
+public class GetCheckingListIncMarkListQuery extends CallableQuery<CheckingListMark> {
+
+    private String checkingListHeadGuid;
+    private List<CheckingListMark> headList;
+    public GetCheckingListIncMarkListQuery(Connection connection, String checkingListHeadGuid) {
+        super(connection);
+        this.checkingListHeadGuid = checkingListHeadGuid;
+        headList = new ArrayList<>();
+    }
+
+    @Override
+    protected void SetQuery() {
+        setSqlString("{? = call V_StoreTSD.dbo.CheckingListMark(?)}");
+    }
+
+    @Override
+    protected void SetQueryParams() throws SQLException {
+        stmt.registerOutParameter(1, Types.INTEGER);
+        stmt.setString(2,checkingListHeadGuid);
+
+    }
+
+    @Override
+    public void Execute() throws SQLException {
+            super.Execute();
+        CheckingListMarkConverter converter = new CheckingListMarkConverter();
+            while (getResultSet().next()) {
+                CheckingListMark head = new CheckingListMark();
+                converter.Convert(getResultSet(),head);
+                headList.add(head);
+            }
+            stmt.getMoreResults();
+            setReturnCode((int)stmt.getObject(1));
+            int r = getReturnCode();
+    }
+
+    public List<CheckingListMark> getHeadList() {
+        return headList;
+    }
+}
