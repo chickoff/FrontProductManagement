ebcb26208d02209c89de91dd71e50a82de56f38b
chickoff <ksandrxxx@gmail.com>
2019-03-25 12:14:31 +0300

    по просьбе трудящихся и жаждущих

---
+++ app/src/main/java/ru/a5x5retail/frontproductmanagement/db/models/CheckingListManufacturerDate.java
@@ -0,0 +1,11 @@
+package ru.a5x5retail.frontproductmanagement.db.models;
+
+import java.util.Date;
+
+public class CheckingListManufacturerDate {
+   public String guid;
+   public String checkingListIncHeadGuid;
+   public int code;
+   public Date manufacturingDate;
+   public int orderBy;
+}
