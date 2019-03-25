d1402e8533e841b862ad9dc759bc067bd5e28d53
chickoff <ksandrxxx@gmail.com>
2019-03-25 12:06:34 +0300

    по просьбе трудящихся и жаждущих

---
+++ app/src/main/java/ru/a5x5retail/frontproductmanagement/db/converters/CheckingListMarkConverter.java
@@ -0,0 +1,40 @@
+package ru.a5x5retail.frontproductmanagement.db.converters;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import ru.a5x5retail.frontproductmanagement.db.models.CheckingListHead;
+import ru.a5x5retail.frontproductmanagement.db.models.CheckingListMark;
+import ru.a5x5retail.frontproductmanagement.interfaces.IDbConvertible;
+
+public class CheckingListMarkConverter implements IDbConvertible<CheckingListMark> {
+    @Override
+    public boolean Convert(ResultSet resultSet, CheckingListMark obj) throws SQLException {
+        obj.guid = resultSet.getObject("GUID").toString();
+        obj.checkingListIncHeadGuid = resultSet.getObject("checkingListIncHeadGuid").toString();
+        obj.code = resultSet.getInt("code");
+        obj.qtyCl = resultSet.getBigDecimal("qtyCl");
+        obj.qtyTtn = resultSet.getBigDecimal("qtyTtn");
+        obj.orderBy = resultSet.getInt("orderBy");
+        return false;
+    }
+
+
+/*    @Override
+    public boolean Convert(ResultSet resultSet, CheckingListHead obj) {
+        try {
+            obj.Guid = resultSet.getObject("GUID").toString();
+            obj.StatusID = resultSet.getObject("StatusID").toString();
+            obj.TypeDocID = resultSet.getObject("TypeDocID").toString();
+            obj.RRGUID = resultSet.getObject("RRGUID").toString();
+            obj.NameDoc = resultSet.getObject("NameDoc").toString();
+            obj.Note = resultSet.getString("Note");
+            obj.IMEI = resultSet.getObject("IMEI").toString();
+            obj.LDM = resultSet.getObject("LDM").toString();
+            obj.LDC = resultSet.getObject("LDC").toString();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }*/
+}
